8fb7c5085df81019ce02382da55df7708ffcc1a0
Anish-Amiti <anish.singh@adesa.com>
2019-03-31 20:53:12 +0530

    Update RainbowApplication.java
    modified

--- rainbow/src/main/java/org/rainbow/finance/RainbowApplication.java
+++ rainbow/src/main/java/org/rainbow/finance/RainbowApplication.java
@@ -16,4 +16,6 @@ public class RainbowApplication extends SpringBootServletInitializer {
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
 		return builder.sources(RainbowApplication.class);
 	}
-}
+
+
+}	 
