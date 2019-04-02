d8ec37805616d3b61404fe77aa96b98758385128
jalajcoolgithub <49112274+jalajcoolgithub@users.noreply.github.com>
2019-04-02 16:57:54 +0530

    created footercontroller

--- rainbow/src/main/java/org/rainbow/finance/controllers/mvc/FooterController.java
+++ rainbow/src/main/java/org/rainbow/finance/controllers/mvc/FooterController.java
@@ -4,10 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class WelcomeController {
+public class FooterController {
 
 	@GetMapping("/footer")
-	public String welcome() {
+	public String footer() {
 		return "footer";
 	}
 
