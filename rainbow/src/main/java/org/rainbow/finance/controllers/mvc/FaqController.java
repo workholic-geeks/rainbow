f7dd3b256c81f2a00bb53a17c00830e60f61a9c1
AMARKUMARIT <49152774+AMARKUMARIT@users.noreply.github.com>
2019-04-29 00:30:19 +0530

    FAQ Pages

---
+++ rainbow/src/main/java/org/rainbow/finance/controllers/mvc/FaqController.java
@@ -0,0 +1,15 @@
+package org.rainbow.finance.controllers.mvc;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FaqController {
+	
+	@GetMapping("/faq.html")
+	public String Faq() {
+		return "faq";
+	}
+
+}
